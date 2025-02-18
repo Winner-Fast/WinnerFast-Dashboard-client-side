5ef1d9c07c3fb028acd6f64b5b0c7d0d4a759521
SalBouhamidi <bouhamidi.sal@gmail.com>
2025-02-18 17:18:19 +0100

    Structure of the project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Sidebar from './Components/Layouts/sidebar';
+
+
+function App() {
+
+  return (
+    <>
+    <div className="App">
+      <div className="AppGlass">
+        <Sidebar/>
+        {/* <MainDash/>
+        <RightSide/> */}
+      </div>
+    </div>  
+    </>
+  )
+}
+
+export default App
